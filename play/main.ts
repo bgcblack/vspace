d73bd3df5081abe16857b184287904caab6ae073
sunyh <572314446@qq.com>
2024-08-07 18:02:11 +0800

    feax: 暂时解决app.use不报错的问题

--- play/main.ts
+++ play/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './app.vue'
+import { default as vspace } from '@vspace/components'
 
 const app = createApp(App)
+app.use(vspace as any, {})
 app.mount('#app')
